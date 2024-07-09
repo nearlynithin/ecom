fea54ebcaea1620d99cb3f791387c6012aefedd0
nearlynithin <nearlynithin@gmail.com>
2024-07-09 17:17:54 +0530

    🐛 fix: <h1> tag changed to <div> since it caused an error: h1 can't be a child of h1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export const page = () => {
   return (
     <>
       <div className="bg-white min-h-screen flex justify-center items-center">
-        <h1 className="mb-9">
+        <div className="mb-9">
           <h1 className="mb-4  font-extrabold text-6xl  text-transparent bg-clip-text bg-gradient-to-r  from-teal-400 to-white">
             Nithin
           </h1>
@@ -17,7 +17,7 @@ export const page = () => {
             It's been fairly simple so far, The Project Structure is very easy
             to understand.
           </p>
-        </h1>
+        </div>
       </div>
     </>
   );
