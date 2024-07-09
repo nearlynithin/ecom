7d1dc9faac8205ba3710c6302b3a99dfdf1e6d96
nearlynithin <nearlynithin@gmail.com>
2024-07-09 17:31:45 +0530

    ✨ feat: /home and / routes are the same

---
+++ src/components/homeComponent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const HomeContent = () => {
+  return (
+    <>
+      <div className="bg-white min-h-screen flex justify-center items-center">
+        <div className="mb-9">
+          <h1 className="mb-4  font-extrabold text-6xl  text-transparent bg-clip-text bg-gradient-to-r  from-teal-400 to-white">
+            Nithin
+          </h1>
+          <p className=" text-gray-800">
+            Hello! My name is Nithin. I'm not much of "Frontend" guy despite
+            having a "design" related background. Can't wait to ditch this and
+            work on the backend instead :)
+          </p>
+          <p className="text-gray-800">
+            It's been fairly simple so far, The Project Structure is very easy
+            to understand.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeContent;
