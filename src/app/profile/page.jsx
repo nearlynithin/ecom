1d2c9e08fd08d40602bb2cb8ce2c77163b35c126
nearlynithin <nearlynithin@gmail.com>
2024-07-13 19:41:52 +0530

    🐛 fix: Profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <h1>Every user can Visit their Profile here</h1>
-      <p>
-        It'll contain information like their past purchases, payment methods,
-        User data etc
-      </p>
+      <div className="bg-white min-h-screen">
+        <h1 className="text-cyan-800 px-20 py-10 font-light text-2xl ">
+          Every user can Visit their Profile here
+        </h1>
+        <p className="text-center text-teal-800 ">
+          It'll contain information like their past purchases, payment methods,
+          User data, etc.
+        </p>
+      </div>
     </>
   );
 };
