c6db5dbaa34bfd81e5553f5f8ae6be6ec31182fb
nearlynithin <nearlynithin@gmail.com>
2024-08-02 18:17:24 +0530

    ✨ feat : Add to car component

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const AddToCart = () => {
+  return (
+    <div>
+      <button
+        class=" bg-blue-300 hover:bg-blue-400 px-5 py-2 text-sm text-white uppercase rounded-lg"
+        type="submit"
+      >
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
