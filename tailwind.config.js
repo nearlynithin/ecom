76b0e8b8999b2df8be01d7885366d38ff0a8c2fe
nearlynithin <nearlynithin@gmail.com>
2024-08-02 13:30:33 +0530

    🔧 chore: import alias

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        ostrich: ["OstrichSans", "sans-serif"],
+        playwrite: ["PlaywriteBEVLG", "sans-serif"],
+      },
     },
   },
   plugins: [],
