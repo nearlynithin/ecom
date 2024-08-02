6c73b04be073286339fa591193e57523acccc353
nearlynithin <nearlynithin@gmail.com>
2024-08-02 15:36:44 +0530

    ✨ feat : Not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page not found</h1>
+      <p>The page you are looking for does not exist</p>
+      <p>This page is created after overiding the default page</p>
+    </div>
+  );
+}
